00e438d44a7756e0f212a30375450c7229c879d9
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-05-27 15:22:29 +0530

    added route for form to post to

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,7 +1,17 @@
-from flask import render_template
+from flask import render_template, jsonify, Response, request
 
 from api import api
 
 @api.route('/')
 def index_route():
     return render_template('index.html')
+
+@api.route(
+    '/api/recruitment',
+    methods=['POST', 'GET']
+)
+def recruitment_route():
+    if request.method == 'GET':
+        return 'POST HERE'
+    elif request.method == 'POST':
+        return Response(201)

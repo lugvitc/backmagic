5eaf54794fbdc15baa663f27b185ceb9772fe021
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-05-29 11:04:14 +0530

    switched db to mysql

--- api/__init__.py
+++ api/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -5,7 +7,15 @@ from flask_cors import CORS
 api = Flask(__name__)
 CORS(api)
 
-api.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+api.config['SQLALCHEMY_DATABASE_URI'] = f"""mysql+mysqlconnector://{
+    os.environ.get('MSYQL_USERNAME')
+}:{
+    os.environ.get('MYSQL_PASSWORD')
+}@{
+    os.environ.get('MYSQL_SERVER')
+}/{
+    os.environ.get('MYSQL_DB')
+}"""
 api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(api)
 
